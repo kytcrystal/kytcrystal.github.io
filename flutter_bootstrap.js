edc0a30d312d432b6f45c89c525ba3c6e024c5f7
kytcrystal <kytcrystal@users.noreply.github.com>
2025-05-09 16:17:11 +0000

    deploy: kytcrystal/crystal_blog@c042e18ddf7b797454a4ba002869a7ad4a0e2747

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2818980273"
+    serviceWorkerVersion: "3385655156"
   }
 });
